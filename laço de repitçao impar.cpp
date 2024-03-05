538b8d0f25e4255a9e0a02805a5e11875d6fa23e
Fr4n05 <160607626+Fr4n05@users.noreply.github.com>
2024-03-05 09:05:38 -0300

    Add files via upload

---
+++ laço de repitçao impar.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int i;
+	
+	printf("numeros impar da sequencia de 1 a 20\n");
+	
+	for (i = 1; i <= 20; i ++){
+		if (i % 2 !=0){
+			printf("%i \n",i);
+	  } 
+	  
+	}
+	return 0; 
+} 
